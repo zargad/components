a385924d3efaca6836751d59748e6bec9bf3d1e5
Thompson Leonard <t0pl0l250@gmail.com>
2025-09-12 18:57:25 +0300

    Add tests for the Channel trait

--- components/src/channels.rs
+++ components/src/channels.rs
@@ -2,3 +2,51 @@ pub trait Channel<T>: std::marker::Copy {
     fn set(&self, value: T) -> Self;
     fn get(&self) -> T;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get() {
+        let channel = get_test_channel();
+        let event: Event = channel.get();
+        assert_eq!(event, channel.event);
+    }
+
+    #[test]
+    fn test_set() {
+        let channel = get_test_channel();
+        let new_event = Event::Exit;
+        let channel = channel.set(new_event);
+        assert_eq!(channel.event, new_event);
+    }
+
+    fn get_test_channel() -> EventChannel {
+        EventChannel {
+            event: Event::Keyboard,
+        }
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    struct EventChannel {
+        event: Event,
+    }
+
+    impl Channel<Event> for EventChannel {
+        fn set(&self, value: Event) -> Self {
+            let mut this = *self;
+            this.event = value;
+            this
+        }
+        fn get(&self) -> Event {
+            self.event
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    enum Event {
+        Keyboard,
+        Exit,
+    }
+}
