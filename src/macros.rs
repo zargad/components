14b7d1032190689905f2d8087250a0c73d25a91e
Thompson Leonard <t0pl0l250@gmail.com>
2025-09-12 12:16:38 +0300

    Add derive(Channels), broken currently

---
+++ src/macros.rs
@@ -0,0 +1,46 @@
+extern crate proc_macro;
+
+use proc_macro2::{TokenStream, Span};
+use quote::quote;
+use syn::{Data, DeriveInput};
+
+#[proc_macro_derive(Channels)]
+pub fn channels(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse_macro_input!(input as DeriveInput);
+    let toks = channels_inner(&ast)
+        .unwrap_or_else(|err| err.to_compile_error());
+    toks.into()
+}
+
+pub fn channels_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
+    let name = &ast.ident;
+    let generics = &ast.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let fields = match &ast.data {
+        Data::Struct(v) => &v.fields,
+        _ => return Err(non_struct_error()),
+    };
+
+    let generated_code = fields.iter().map(|f| (f.clone().ident.expect("struct fields have names"), f.ty.clone())).map(|(field, ty)| {
+        quote! {
+            impl #impl_generics Channel<#ty> for #name #ty_generics #where_clause {
+                fn set(&self, value: #ty) -> Self {
+                    let mut clone = self.clone();
+                    clone.#field = value;
+                    clone
+                }
+                fn get(&self) -> #ty {
+                    self.#field
+                }
+            }
+        }
+    });
+    Ok(quote! {
+        #(#generated_code)*
+    })
+}
+
+pub fn non_struct_error() -> syn::Error {
+    syn::Error::new(Span::call_site(), "This macro only supports structs.")
+}
