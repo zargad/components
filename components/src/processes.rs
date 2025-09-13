7cf06df7b71b6d8788bcb838985e1035ed7f3624
Thompson Leonard <t0pl0l250@gmail.com>
2025-09-13 10:48:43 +0300

    Add struct Assign

--- components/src/processes.rs
+++ components/src/processes.rs
@@ -1,3 +1,25 @@
-trait Process<C> {
+use crate::channels::Channel;
+
+pub trait Process<C> {
     fn process(&self, channel: C) -> C;
+}
+
+pub struct Assign<T> {
+    value: T,
+}
+
+impl<T> Assign<T> {
+    pub fn new(value: T) -> Self {
+        Self { value }
+    }
+}
+
+impl<T, C> Process<C> for Assign<T> 
+where
+    T: std::marker::Copy,
+    C: Channel<T>,
+{
+    fn process(&self, channel: C) -> C {
+        channel.set(self.value)
+    }
 }
