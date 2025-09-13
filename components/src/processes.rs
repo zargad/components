3a71895bc2ae9f2e73b4ee5d8454c6414ceb5c79
Thompson Leonard <t0pl0l250@gmail.com>
2025-09-13 11:04:47 +0300

    Add struct Map

--- components/src/processes.rs
+++ components/src/processes.rs
@@ -1,3 +1,5 @@
+use std::marker::Copy;
+use std::marker::PhantomData;
 use crate::channels::Channel;
 
 pub trait Process<C> {
@@ -16,10 +18,42 @@ impl<T> Assign<T> {
 
 impl<T, C> Process<C> for Assign<T> 
 where
-    T: std::marker::Copy,
+    T: Copy,
     C: Channel<T>,
 {
     fn process(&self, channel: C) -> C {
         channel.set(self.value)
+    }
+}
+
+pub struct Map<A, B, F> 
+where
+    F: Fn(A) -> B,
+{
+    map: F,
+    phantom_a: PhantomData<A>,
+    phantom_b: PhantomData<B>,
+}
+
+impl<A, B, F> Map<A, B, F>  
+where
+    F: Fn(A) -> B,
+{
+    pub fn new(map: F) -> Self {
+        Self { map, phantom_a: PhantomData, phantom_b: PhantomData }
+    }
+}
+
+impl<A, B, F, C> Process<C> for Map<A, B, F> 
+where
+    A: Copy,
+    B: Copy,
+    F: Fn(A) -> B,
+    C: Channel<A> + Channel<B>,
+{
+    fn process(&self, channel: C) -> C {
+        let a: A = channel.get();
+        let b: B = (self.map)(a);
+        channel.set(b)
     }
 }
