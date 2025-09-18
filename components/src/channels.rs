d277e4aed599269a9d42a39044f4d8028bd24770
Thompson Leonard <t0pl0l250@gmail.com>
2025-09-18 21:48:23 +0300

    Replace trait Channel and DuelChannel with ChannelGet and ChannelSet (not respectively)

--- components/src/channels.rs
+++ components/src/channels.rs
@@ -1,18 +1,14 @@
 use std::marker::Copy;
 
-pub trait Channel<T>: Copy {
+pub trait ChannelGet<T>: Copy {
     fn get(&self) -> T;
+}
+
+pub trait ChannelSet<T>: Copy {
     fn set(&self, value: T) -> Self;
 }
 
-pub trait DuelChannel<A, B>: Copy + Channel<A> + Channel<B> {
-    fn duel_get(&self) -> A {
-        self.get()
-    }
-    fn duel_set(&self, value: B) -> Self {
-        self.set(value)
-    }
-}
+// NOTE: might wanna add ChannelMutSet
 
 #[cfg(test)]
 mod tests {
@@ -33,21 +29,6 @@ mod tests {
         assert_eq!(channel.event, new_event);
     }
 
-    #[test]
-    fn test_duel_get() {
-        let channel = get_test_channel();
-        let event: Event = channel.duel_get();
-        assert_eq!(event, channel.event);
-    }
-
-    #[test]
-    fn test_duel_set() {
-        let channel = get_test_channel();
-        let new_event = Event::Exit;
-        let channel = channel.duel_set(new_event);
-        assert_eq!(channel.event, new_event);
-    }
-
     const fn get_test_channel() -> EventChannel {
         EventChannel {
             event: Event::Keyboard,
@@ -59,18 +40,19 @@ mod tests {
         event: Event,
     }
 
-    impl Channel<Event> for EventChannel {
-        fn set(&self, value: Event) -> Self {
-            let mut copy = *self;
-            copy.event = value;
-            copy
-        }
+    impl ChannelGet<Event> for EventChannel {
         fn get(&self) -> Event {
             self.event
         }
     }
 
-    impl DuelChannel<Event, Event> for EventChannel {}
+    impl ChannelSet<Event> for EventChannel {
+        fn set(&self, value: Event) -> Self {
+            let mut copy = *self;
+            copy.event = value;
+            copy
+        }
+    }
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     enum Event {
